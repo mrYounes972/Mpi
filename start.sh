a921b670e8d838720b8679cd3c7a289a36398f99
mrYounes972 <f15277839@gmail.com>
2025-07-24 19:44:09 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-python telegram_bot.py  # یا dashboard.py بسته به فایل اجرایی
+python telegram_bot.py   # یا dashboard.py بسته به پروژه‌ات
